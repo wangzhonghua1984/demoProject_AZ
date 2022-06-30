077e911ec609a6063aece3aeb6210228ffcd3dbb
Gogs <gogs@fake.local>
2022-07-01 00:17:47 +0800

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "RGName" {
+  type = string
+}
+
+
+variable "Location" {
+  type = string
+}
+
+
+variable "SubscriptionID" {
+  type = string
+}
+
+
+variable "ClientID" {
+  type = string
+}
+
+
+variable "ClientSecret" {
+  type = string
+}
+
+
+variable "TenantID" {
+  type = string
+}
